b732aa799e25a29aefeb4c143a50ae605b2221d4
akagiyuu <khanghuynh0245@gmail.com>
2025-06-28 07:56:13 +0700

    feat: migrate to nextjs

--- frontend/src/router/AppRouter.tsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-import routes from './routes';
-
-const AppRouter = () => {
-    const element = useRoutes(routes);
-    return element;
-};
-
-export default AppRouter;
