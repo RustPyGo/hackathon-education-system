7dc9d88f276ca62b4a0d82efe2ca82c448c0084d
akagiyuu <khanghuynh0245@gmail.com>
2025-06-28 12:12:54 +0700

    feat(auth): require auth from except for home

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,5 +1,8 @@
 import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
-export default authkitMiddleware();
-
-export const config = { matcher: ['/'] };
+export default authkitMiddleware({
+    middlewareAuth: {
+        enabled: true,
+        unauthenticatedPaths: ['/'],
+    },
+});
